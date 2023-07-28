da197054515981b2f06b86c3f9d9e479c7feff64
Xuesong Peng <pengxuesong.cn@gmail.com>
2023-07-28 19:55:50 +0800

    feat: support legacy librime

---
+++ schema/rime.lua
@@ -0,0 +1,6 @@
+smart_selector = require("jiandao/smart_selector")
+topup_selector = require("jiandao/topup_selector")
+date_time_translator = require("jiandao/date_time_translator")
+calculator = require("jiandao/calculator")
+unicode_translator = require("jiandao/unicode_translator")
+hint_filter = require("jiandao/hint_filter")
